421ad9e36460afe33108d53f3df30243ca4f3ca7
luketudor <Luke.Tudor@myob.com>
2017-11-23 12:20:23 +1300

    Added neccessary methods for calculator class

--- PayslipGenerator/PayslipGenerator/Calculator.cs
+++ PayslipGenerator/PayslipGenerator/Calculator.cs
@@ -1,11 +1,49 @@
-﻿namespace PayslipGenerator.Tests
+﻿using System;
+
+namespace PayslipGenerator
 {
     public class Calculator
     {
 
-        public double GrossIncome(int annualSalary, int numberOfMonths)
+        public double GrossIncome(double annualSalary, double numberOfMonths)
         {
-            return 1000;
+            return Math.Round(annualSalary / numberOfMonths);
+        }
+
+        public double IncomeTax(double grossIncome)
+        {
+            var tax = 0.0;
+            if (grossIncome <= 18200)
+            {
+                tax = 0;
+            }
+            else if (grossIncome <= 37000)
+            {
+                tax = (grossIncome - 18200) * 0.19;
+            }
+            else if (grossIncome <= 80000)
+            {
+                tax = (grossIncome - 37000) * 0.325 + 3572;
+            }
+            else if (grossIncome <= 180000)
+            {
+                tax = (grossIncome - 80000) * 0.37 + 17547;
+            }
+            else
+            {
+                tax = (grossIncome - 180000) * 0.45 + 54547;
+            }
+            return Math.Round(tax);
+        }
+
+        public double NetIncome(double grossIncome, double incomeTax)
+        {
+            return grossIncome - incomeTax;
+        }
+
+        public double Super(double grossIncome, double superRate)
+        {
+            return Math.Round(grossIncome * superRate);
         }
     }
 }
--- PayslipGenerator/PayslipGenerator/CalculatorShould.cs
+++ PayslipGenerator/PayslipGenerator/CalculatorShould.cs
@@ -6,6 +6,7 @@ namespace PayslipGenerator.Tests
     [TestFixture]
     public class CalculatorShould
     {
+        
         [Test]
         public void ReturnGrossIncomeForSalaryTwelveThousand()
         {
@@ -16,6 +17,161 @@ namespace PayslipGenerator.Tests
             var expectedIncome = 1000;
 
             Assert.AreEqual(expectedIncome, calculator.GrossIncome(annualSalary, numberOfMonths));
+        }
+
+        [Test]
+        public void ReturnGrossIncomeForSalaryTwentyFourThousand()
+        {
+            var annualSalary = 24000;
+            var numberOfMonths = 12;
+            var calculator = new Calculator();
+
+            var expectedIncome = 2000;
+
+            Assert.AreEqual(expectedIncome, calculator.GrossIncome(annualSalary, numberOfMonths));
+        }
+
+        [Test]
+        public void ReturnRoundedDownGrossIncomeForSalaryTenThousand()
+        {
+            var annualSalary = 10000;
+            var numberOfMonths = 12;
+            var calculator = new Calculator();
+
+            // 833.33333 to be more precise
+            var expectedIncome = 833;
+
+            Assert.AreEqual(expectedIncome, calculator.GrossIncome(annualSalary, numberOfMonths));
+        }
+
+        [Test]
+        public void ReturnRoundedUpGrossIncomeForSalarySeven()
+        {
+            var annualSalary = 7;
+            var numberOfMonths = 12;
+            var calculator = new Calculator();
+
+            // 0.5833333 to be more precise
+            var expectedIncome = 1;
+
+            Assert.AreEqual(expectedIncome, calculator.GrossIncome(annualSalary, numberOfMonths));
+        }
+
+        [Test]
+        public void ReturnIncomeTaxForFirstTaxBracket()
+        {
+            var grossIncome = 10000;
+            var calculator = new Calculator();
+
+            var expectedTax = 0;
+
+            Assert.AreEqual(expectedTax, calculator.IncomeTax(grossIncome));
+        }
+
+        [Test]
+        public void ReturnIncomeTaxForSecondTaxBracket()
+        {
+            var grossIncome = 20000;
+            var calculator = new Calculator();
+
+            var expectedTax = 342;
+
+            Assert.AreEqual(expectedTax, calculator.IncomeTax(grossIncome));
+        }
+
+        [Test]
+        public void ReturnIncomeTaxForThirdTaxBracket()
+        {
+            var grossIncome = 40000;
+            var calculator = new Calculator();
+
+            var expectedTax = 4547;
+
+            Assert.AreEqual(expectedTax, calculator.IncomeTax(grossIncome));
+        }
+
+        [Test]
+        public void ReturnIncomeTaxForFourthTaxBracket()
+        {
+            var grossIncome = 90000;
+            var calculator = new Calculator();
+
+            var expectedTax = 21247;
+
+            Assert.AreEqual(expectedTax, calculator.IncomeTax(grossIncome));
+        }
+
+        [Test]
+        public void ReturnIncomeTaxForFifthTaxBracket()
+        {
+            var grossIncome = 200000;
+            var calculator = new Calculator();
+
+            var expectedTax = 63547;
+
+            Assert.AreEqual(expectedTax, calculator.IncomeTax(grossIncome));
+        }
+
+        [Test]
+        public void ReturnRoundedIncomeTaxForGrossOneTwoThreeFourFiveSix()
+        {
+            var grossIncome = 123456;
+            var calculator = new Calculator();
+
+            // 33625.72 to be exact
+            var expectedTax = 33626;
+
+            Assert.AreEqual(expectedTax, calculator.IncomeTax(grossIncome));
+        }
+
+        [Test]
+        public void ReturnNetIncomeForGrossTwoThousand()
+        {
+            var grossIncome = 2000;
+            var incomeTax = 500;
+            var calculator = new Calculator();
+
+            var expectedIncome = 1500;
+
+            Assert.AreEqual(expectedIncome, calculator.NetIncome(grossIncome, incomeTax));
+        }
+
+        [Test]
+        public void ReturnNetIncomeForGrossFiveFourThreeTwoOne()
+        {
+            var grossIncome = 54321;
+            var incomeTax = 14417;
+            var calculator = new Calculator();
+
+            var expectedIncome = 39904;
+
+            Assert.AreEqual(expectedIncome, calculator.NetIncome(grossIncome, incomeTax));
+        }
+
+        [Test]
+        public void ReturnSuperForRandomGross()
+        {
+            var grossIncome = 4735;
+            var superRate = 0.1226;
+            var calculator = new Calculator();
+
+            // 580.511 to be exact
+            var expectedIncome = 581;
+
+            Assert.AreEqual(expectedIncome, calculator.Super(grossIncome, superRate));
+        }
+
+        [Test]
+        public void ReturnRoundedDownSuperForRandomGross()
+        {
+            var grossIncome = 12944;
+            var superRate = 0.0765;
+            var calculator = new Calculator();
+
+            // 990.216 to be exact
+            var expectedIncome = 990;
+
+            Assert.AreEqual(expectedIncome, calculator.Super(grossIncome, superRate));
         }
     }
 }
