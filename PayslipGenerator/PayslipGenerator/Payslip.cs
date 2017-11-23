18b0a9e5cee5945a27562da40980c59009fde397
luketudor <Luke.Tudor@myob.com>
2017-11-23 15:21:53 +1300

    Created first payslip test and associated class

---
+++ PayslipGenerator/PayslipGenerator/Payslip.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PayslipGenerator
+{
+    public class Payslip
+    {
+        private string firstName;
+        private string lastName;
+        private object annualSalary;
+        private object superRate;
+        private object paymentStartDate;
+
+        public Payslip(string firstName, string lastName, object annualSalary, object superRate, object paymentStartDate)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.annualSalary = annualSalary;
+            this.superRate = superRate;
+            this.paymentStartDate = paymentStartDate;
+        }
+
+        public string Name()
+        {
+            return "Foo Bar";
+        }
+    }
+}
---
+++ PayslipGenerator/PayslipGenerator/Tests/PayslipShould.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace PayslipGenerator.Tests
+{
+    [TestFixture]
+    public class PayslipShould
+    {
+
+        [Test]
+        public void ReturnsFirstAndLastName() 
+        {
+            var firstName = "Foo";
+            var lastName = "Bar";
+            var payslip = new Payslip(firstName, lastName, null, null, null);
+
+            var expectedName = "Foo Bar";
+
+            Assert.AreEqual(expectedName, payslip.Name());
+        }
+    }
+}
